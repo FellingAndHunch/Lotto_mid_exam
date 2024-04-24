4cc0fb1e6de12f467a5150f4bea17f379fac6201
YJ <gg3582@naver.com>
2024-04-24 14:32:21 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/lotto_mid_exam/LottoMidExamApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lotto_mid_exam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LottoMidExamApplication
+
+fun main(args: Array<String>) {
+    runApplication<LottoMidExamApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/lotto_mid_exam/LottoMidExamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.lotto_mid_exam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LottoMidExamApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
